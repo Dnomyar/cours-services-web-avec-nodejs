cce08f96517c7eaaa0333ec301230212367a90fe
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-12 10:56:17 +0200

    Exo 2 done

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const commander = require('commander');
+
 /**
  * @type {string}
  */
@@ -6,6 +8,21 @@ const contactFile = process.env.npm_package_config_contacts;
 const contacts = require(contactFile);
 
 
-contacts
-  .map(row => `${row.lastName.toUpperCase()} ${row.firstName}`)
-  .forEach(row => console.log(row));
+const showContacts = () => {
+  contacts
+    .map(row => `${row.lastName.toUpperCase()} ${row.firstName}`)
+    .forEach(row => console.log(row));
+};
+
+
+commander
+  .command('list')
+  .description('List contacts')
+  .action(() => {
+    showContacts();
+  });
+
+
+if (process.argv.length <= 2) commander.help();
+
+commander.parse(process.argv);
