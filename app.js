80871a8d9f277de8c867dee7d3af731e664c21c7
Damien RAYMOND <dam.ray49@gmail.com>
2017-04-12 10:40:55 +0200

    Exo 1 done

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+/**
+ * @type {string}
+ */
 const contactFile = process.env.npm_package_config_contacts;
+
+const contacts = require(contactFile);
+
+
+contacts
+  .map(row => `${row.lastName.toUpperCase()} ${row.firstName}`)
+  .forEach(row => console.log(row));
